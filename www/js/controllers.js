6a127a98f36a55100a578b5375fc5d5e1a4b2b25
shrutiLetsgomo <shruti.aggarwal@letsgomo.com>
2015-02-27 15:01:41 +0530

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,103 @@
+angular.module('starter.controllers', ['starter.services'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function(loginData, isValid){
+    if(isValid){
+      
+      console.log(loginData.username +"login now");
+    
+    } else {
+      
+     // $scope.loginForm.username.$pristine = false;
+     // $scope.loginForm.password.$pristine = false;
+    }
+  };
+ /* $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+*/
+  $scope.fbLogin = function() {
+    openFB.login(
+        function(response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        },
+        {scope: 'email,publish_actions'});
+}
+})
+
+.controller('SessionsCtrl', function($scope,$stateParams, Session) {
+    $scope.sessions = Session;
+})
+
+.controller('SessionCtrl', function($scope, $stateParams, Session) {
+  for(var i=0;i< Session.length;i++)
+  {
+    if((Session[i]).id == $stateParams.sessionId)
+    {
+      $scope.session = Session[i];
+      return;
+    }
+  }
+   //$scope.session = Session.get({sessionId: $stateParams.sessionId});
+})
+
+.controller('ProfileCtrl', function($scope) {
+    openFB.api({
+        path: '/me',
+        params: {fields: 'id,name'},
+        success: function(user) {
+            $scope.$apply(function() {
+                $scope.user = user;
+            });
+        },
+        error: function(error) {
+            alert('Facebook error: ' + error.error_description);
+        }
+    });
+})
+
+.controller('LoginCtrl', function($scope) {
+  $scope.login = function(loginData, isValid){
+    if(isValid){
+      
+      console.log(loginData.username +"login now");
+    
+    } else {
+      
+    //  $scope.loginForm.username.$pristine = false;
+    //  $scope.loginForm.password.$pristine = false;
+    }
+  };
+});
